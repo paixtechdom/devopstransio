d564dbeae7942ffaf663fdd67e4adac43197fd72
paixtechdom <paixtechdom@gmail.com>
2024-08-03 04:32:19 +0100

    removed unused imports

--- src/assets/components/Card.tsx
+++ src/assets/components/Card.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react"
-import { Link } from "react-router-dom"
 import { Button } from "./Button"
 import { useDispatch } from "react-redux"
 import { setCurrentService } from "../store/navigation/navigationSlice"
